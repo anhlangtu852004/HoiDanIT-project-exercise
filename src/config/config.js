c57a256e655a039201caef64bfb8adde00a87145
tri <tridesignnm@gmal.com>
2022-08-18 13:08:32 +0700

    ket noi database

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 {
   "development": {
     "username": "root",
-    "password": "minhtri404",
-    "database": "hoidanit",
+    "password": null,
+    "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
